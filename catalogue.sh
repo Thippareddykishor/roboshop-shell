82ab8715212335be6892a7bcfa293e640b318448
kishor <kishorebsc12@gmail.com>
2025-11-21 11:04:15 +0530

    added

---
+++ catalogue.sh
@@ -0,0 +1,19 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+
