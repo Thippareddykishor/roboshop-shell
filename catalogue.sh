6f3e382ce80160c536c062645ec95866d3e2ff16
kishor <kishorebsc12@gmail.com>
2025-11-21 11:32:55 +0530

    added

--- catalogue.sh
+++ catalogue.sh
@@ -4,6 +4,7 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 
 mkdir /app
